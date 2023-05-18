fbfceed39b2715c4c8fb0087a14b712775e12213
jmpsia <126020968+jmpsia@users.noreply.github.com>
2023-05-18 13:25:51 -0700

    initial files for project

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import psycopg2
-from flask import Flask
+import math
+from flask import Flask, request, render_template
 from psycopg2.extras import RealDictCursor
 
 conn = psycopg2.connect(
@@ -7,7 +8,64 @@ conn = psycopg2.connect(
     cursor_factory=RealDictCursor)
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_world():
     return "Hello, World!"
+
+
+@app.route("/set")
+def render_sets():
+    year = request.args.get("year", type=int)
+    rank = request.args.get("rank", type=int)
+    country = request.args.get("country", "")
+    score = request.args.get("score", type=float)
+    gdp = request.args.get("gdp", type=float)
+    life_expectancy = request.args.get("life_expectancy", type=float)
+    freedom = request.args.get("freedom", type=float)
+    generosity = request.args.get("generosity", type=float)
+    corruption = request.args.get("corruption", type=float)
+    social_support = request.arg.get("social_support", type=float)
+
+    #things we will need for the website..
+    #sort_by = request.args.get("sort_by", "set_name")
+    #sort_dir = request.args.get("sort_dir", "asc")
+    #no pagination please
+
+    params = {
+        "year": year
+        "rank": rank
+        "country": country
+        "score": score
+        "gdp": gdp
+        "life_expectancy": life_expectancy
+        "freedom": freedom
+        "generosity": generosity
+        "corruption": corruption
+        "social_support": social_support
+
+        #"sort_by": sort_by
+        #"sort_dir": sort_dir
+
+    }
+
+    from_where_clause =
+        #INSERT SQL CODE
+        
+    #define methods we need for the variables, default
+
+    with conn.cursor() as cur:
+        cur.execute()#INSERTCODE)
+        results = list(cur.fetchall())
+        cur.execute()#INSERTCODE)
+        count = cur.fetchone()["count"]
+
+
+    return render_template(
+        "sets.html",
+        params=request.args,
+        result_count=count,
+        sets=results)
+
+}
+
+
