4f1a35ccd3ca5cef41440fb733c25cc298780471
jmpsia <126020968+jmpsia@users.noreply.github.com>
2023-05-22 14:56:46 -0700

    sql file

---
+++ .devcontainer/pg_init/TODO.sql
@@ -0,0 +1,12 @@
+create table happiness (
+    year int,
+    rank int,
+    country text,
+    score text,
+    gdp text,
+    life_expectancy text,
+    freedom text,
+    generosity text,
+    corruption text,
+    social_support text
+);
